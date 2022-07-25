2e941603f6849c3aff8043eac2d11360c0b1e922
Diego Valcarce <dvalcarcer@gmail.com>
2022-07-25 11:51:41 +0200

    Neovim basics

---
+++ .config/nvim/lua/user/options.lua
@@ -0,0 +1,40 @@
+local options = {
+    backup = false,
+    clipboard = "unnamedplus",
+    cmdheight = 2,
+    conceallevel = 0,
+    fileencoding = "utf-8",
+    hlsearch = true,
+    ignorecase = true,
+    mouse = "a",
+    pumheight = 10,
+    showtabline = 2,
+    smartcase = true,
+    smartindent = true,
+    splitbelow = true,
+    splitright = true,
+    swapfile = false,
+    timeoutlen = 100,
+    undofile = true,
+    updatetime = 300,
+    writebackup = false,
+    expandtab = true,
+    shiftwidth = 4,
+    tabstop = 4,
+    cursorline = true,
+    number = true,
+    relativenumber = false,
+    signcolumn = "yes",
+    wrap = true,
+    scrolloff = 8,
+    sidescrolloff = 8,
+}
+
+vim.opt.shortmess:append "c"
+
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
+
+vim.cmd "set whichwrap+=<,>,[,],h,l"
+vim.cmd [[set iskeyword+=-]]
